72d4a0272bf492470f120a6306b66400dd569c4a
Dmitriymush <powder322@gmail.com>
2019-10-28 16:26:23 +0200

    Updates

---
+++ precache-manifest.9243984a463dec4b0120ae021679edf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66d3e1a027b29b1a2fc25ffd8d474b6",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "9fd31ad83dc3b7ade514",
+    "url": "/react_static-list-of-todos/static/css/main.61ddce57.chunk.css"
+  },
+  {
+    "revision": "f85a0b25dd9272fe18b5",
+    "url": "/react_static-list-of-todos/static/js/2.cbae395a.chunk.js"
+  },
+  {
+    "revision": "9fd31ad83dc3b7ade514",
+    "url": "/react_static-list-of-todos/static/js/main.5cd4ca27.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
